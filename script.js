8ece3895e8ca38a1cbb141e4612ef5fa76595003
Meetahir1 <meetahirgit@gmail.com>
2024-09-16 00:39:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,241 @@
+let arr = [
+    {
+        src: "https://img10.hotstar.com/image/upload/f_auto,q_90,w_1200/sources/r1/cms/prod/6689/1722084336689-i",
+        nameSrc: "https://img10.hotstar.com/image/upload/f_auto,h_148/sources/r1/cms/prod/1183/1722084351183-t",
+        video: "https://imdb-video.media-imdb.com/vi1961018649/1434659607842-pgv4ql-1670936785983.mp4?Expires=1726506538&Signature=gv9UJFeKMlxLOzjFC5B-asUaXlwlpEQ6-u0FI22lLKHrVYrJ29INlCmslUgUpcE4o8s~WDaKTq0OuZOfEDxE-4Z7bLBJWYvLJoXhryw~yQTOXY~SSmXDKum-aGjppaUkF6qCCuBdnSLkQw7M4UHAhRblebviCCjvlIbyt8nvKqPu59qoShS7IekKtJcXP7wD7kca8lYiRDvV0HNIJRIMW9vySk-BDo4lQC64URJxOZpte9Uk934Jec7rpIMKbtulgkUuETpIwrnv9Qtyb-7S8Lk53CWBAnmLAxpAkDuDfkPiY--gKBzTcDvu0Wk1tCBkqw8i7zUC9Ueka2T4Rmq4Rw__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
+        movie_name: "Ved",
+    },
+    {
+        src: "https://img10.hotstar.com/image/upload/f_auto,q_90,w_1200/sources/r1/cms/prod/5909/1721644845909-i",
+        nameSrc: "https://img10.hotstar.com/image/upload/f_auto,h_148/sources/r1/cms/prod/2817/1721644852817-t",
+        video: "https://imdb-video.media-imdb.com/vi2004861465/1434659607842-pgv4ql-1661274930160.mp4?Expires=1726505944&Signature=Athjd-YwIsELhkO~7r5PKgvGA5b538dDPnqn3-r2NoyoNq9FbVbXWutojUX8l5K1ydZ8V0OgwZ8Kwi0VydNHDFNrrQ58wOS~SVy~TpfGMLU15u6-NOOS-KH~31o8HZHaCThHSyt52XyY5pyMKnLtLIgruFVlYuHolqd~FfVSruVcy~4Tsg3JUmbclHQyij1V8Lz3mVbtFHlO4MGtPnyD4FrlEG8Iv4R~I4yiuk2m3vOP6JOjdXkyhiOVXdLVnvwEUUy1OpDu22H-iYm508oU5wUtNgbfMTHlSyXvgyuKIcuIMfHQ~r2jBe7W94fVPxhS9kBMsHX2JiBj~o2-VfL-nQ__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
+        movie_name: "Bramhashtra"
+    },
+    {
+        src: "https://img10.hotstar.com/image/upload/f_auto,q_90,w_1920/sources/r1/cms/prod/3694/1725561503694-i",
+        nameSrc: "https://img10.hotstar.com/image/upload/f_auto,h_148/sources/r1/cms/prod/7820/1725561517820-t",
+        video: "https://imdb-video.media-imdb.com/vi1295566361/1434659607842-pgv4ql-1718641096130.mp4?Expires=1726503216&Signature=F8jvbH4N0fUYx7UKBo7g0HgFQGAhDUXqatdCcwyzK4KVkImAa2GuEq6qeJm0ayTMCo4A~TyuSZWdHMLTzOnmsICmP2jdzS9WVbw67NYpni~9Z4Ol5Ec~qBqSpm12AhlOBeVJc6AYtUvONqFirmkXFdzaqsbIl42v1yZ6GH056UTG-U3PrphgwdL40t~yU82K-rHV~-Mies-35Vt9jMPNZii2NbMYBPBEOpDgBS0bcT9P56mIMHmn9RcOuJIlKB3zhM3ma7LudmRZ29ds4GtgO-wyhaCCLq-wgW0iV3gONvPYr-Z8ez0Od89x3NzJG8R5I4XhvftEJI1y6CYb-bCb4g__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
+        movie_name: "Kill"
+    },
+    {
+        src: "https://img10.hotstar.com/image/upload/f_auto,q_90,w_1200/sources/r1/cms/prod/6482/1721630406482-i",
+        nameSrc: "https://img10.hotstar.com/image/upload/f_auto,h_148/sources/r1/cms/prod/6453/1721630416453-t",
+        video: "https://imdb-video.media-imdb.com/vi4251042329/1434659607842-pgv4ql-1661417593079.mp4?Expires=1726506064&Signature=szczIyD7swNmTFO1geCTA9eoBIaMR7DaxipcpPMli-72wr8gt9XthBhlnKZJ7K3VwtaptSIHZOSYKxLHqoRfu7KaPHR3z4nlnG9KYiIHtBcCI0x1fxClZFPt6jXxmlP63Zay-I1MqcdA3XQOpKP0RiifTrCuRqP3rBbZ1oGyIkQy0ijxJYw24WZforA2wZYyCL~RVDq4HhQkUIpnx64R-cT2vEvsjZx2ot~h96i6ilQXdNOX4IfwLGUQtDefrao8ZAOU59gb4eZBeXwto03s04rZTmfJY0Q8MLFIxXdY4tiESh--SxZ0lBePgaIuyzNwXYd8TcV5x26auH1DuRESSQ__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
+        movie_name: "Cuttputlli"
+    },
+    {
+        src: "https://img10.hotstar.com/image/upload/f_auto,q_90,w_1200/sources/r1/cms/prod/1686/1724393351686-i",
+        nameSrc: "https://img10.hotstar.com/image/upload/f_auto,h_148/sources/r1/cms/prod/5519/1724393365519-t",
+        video: "https://imdb-video.media-imdb.com/vi1009305113/1434659607842-pgv4ql-1720019016822.mp4?Expires=1726506417&Signature=tfL1Zvi5VJ2tH0r1qTEqXgqGeIE~DUTbvXAoI4k2nvmelv6LHV4jrGxNB17Xt-SUPZhwnZ7RAJLksWFa0QdTlTGgG5vRId9mN0lmUtEkst0bY27H98GbLysCPxq0eKLzEjnC52Y2XutjXImuPEKExrYIw0Zv1e83mEFA12EK1jabSKnxjU4u9XKL9H5JYPpnnfO-x0XvDRLDERzPle0X7hUnO331bUICDt2PY1WZJh5bNgqz6SeCJeX0j2~iVmDWyEO0-VjXK8nIyb48g1jEO2aV8u2ondUk91MoxJAbT4BvWiO7iQRWKqLWEU4gALxvQn06VQMpD9-28BIyDt9N3w__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
+        movie_name: "Commander karan saxena"
+    },
+    {
+        src: "https://img10.hotstar.com/image/upload/f_auto,q_90,w_1200/sources/r1/cms/prod/7032/1721638037032-i",
+        nameSrc: "https://img10.hotstar.com/image/upload/f_auto,h_148/sources/r1/cms/prod/9125/1721638049125-t",
+        video: "https://imdb-video.media-imdb.com/vi1381809945/1434659607842-pgv4ql-1580969277851.mp4?Expires=1726506222&Signature=bz4IWeEVDO59LfAijN5PbCUJDUXsGGfDDbZrXckC~6iXCul3KQBmla5e1EVuYBQts4c0t~Hw70KkW6W8nsF1DsBCD1wRw66dBJcs8otWzGizaZKzw5LZZc7W7Z2wIlrmcIm5M5lSHUcCTdU~rBmtHJ-cIdxDVzFceAbUJBJMwtC4k-uRJYwJOXD8szvZvcRNWnGP0abpNdXdnG5KRrWO~QIXJciYkJsMhshukl579cgj7EeTKEBkDxlgu9UlAjizpvs0i4qWbZBX1Mwf0CGHXwtOupia0UuZT0A5yUUQujimuj~0uqyiKfZCNVVmZL0UvhNbYintM~dzs55bIi-N~A__&Key-Pair-Id=APKAIFLZBVQZ24NQH3KA",
+        movie_name: "Baaghi3"
+    }
+]
+
+const tl = gsap.timeline();
+
+const img_cont = document.querySelector("#img_cont");
+const text_cont = document.querySelector("#text_cont");
+const btm1 = document.querySelector("#btm1");
+
+function BtmDataInput() {
+    let clutter = "";
+    arr.forEach(function (obj, idx) {
+        clutter += `<img id="${idx}" class="align" src="${obj.src}">`;
+    })
+    btm1.innerHTML = clutter;
+}
+
+BtmDataInput();
+
+tl.from(".transform", {
+    x: "-100%",
+    opacity: 0,
+    ease: "Power4.easeInOut",
+    stagger: .2
+})
+
+tl.from("#btm2 i", {
+    y: "100%",
+    duration: 1,
+    stagger: .2,
+    ease: "Power4.easeInOut"
+})
+
+tl.to(".align", {
+    y: "0%",
+    delay: -.8,
+    duration: .8,
+    stagger: .2,
+    ease: "sine.in"
+})
+
+
+let currentIndex = 0;
+let interval;
+
+function startInterval() {
+    interval = setInterval(() => {
+        blur();
+        select();
+        coming();
+        going();
+
+    }, 4000)
+}
+
+startInterval();
+
+
+function coming() {
+    img_cont.innerHTML = `<img id="img" src="${arr[currentIndex].src}">`;
+    text_cont.innerHTML = `<img id="nameImage" src="${arr[currentIndex].nameSrc}">`;
+    currentIndex >= arr.length - 1 ? currentIndex = 0 : currentIndex++;
+    setTimeout(() => {
+        gsap.to("#img", {
+            x: "0%",
+            duration: 1,
+            ease: "Power4.easeInOut",
+            function() {
+                gsap.to("#main", {
+                    backgroundColor: "black"
+                })
+            }
+        })
+
+        gsap.to("#nameImage", {
+            x: "0%",
+            duration: 1,
+            ease: "Power4.easeInOut"
+        })
+    }, 100);
+}
+
+function going() {
+    gsap.to("#img", {
+        x: "100%",
+        duration: 1,
+        delay: 3,
+        ease: "Power4.easeInOut"
+    })
+
+    gsap.to("#nameImage", {
+        x: "100%",
+        duration: 1,
+        delay: 3,
+        ease: "Power4.easeInOut"
+    })
+}
+
+function select() {
+    gsap.to(btm1.childNodes[currentIndex], {
+        borderColor: "white",
+        opacity: 1,
+    })
+}
+
+function blur() {
+    gsap.to(btm1.children, {
+        border: "1px solid transparent",
+        opacity: .6
+    })
+}
+
+let slidingIndex = 0;
+const arrows = document.querySelectorAll(".arrow");
+arrows.forEach(function (tag) {
+    tag.addEventListener("click", function (elem) {
+        if (elem.target.classList.contains("arrow2")) {
+            let lastImage = btm1.childNodes[arr.length - 1].getBoundingClientRect();
+            if (lastImage.right > elem.target.getBoundingClientRect().left) {
+                slidingIndex++;
+                gsap.to("#btm1", {
+                    x: `-${lastImage.width * slidingIndex}`,
+                    function() {
+                        gsap.to("#left", {
+                            display: "flex",
+                        })
+                    }
+                })
+            }
+        }
+        else if (elem.target.classList.contains("arrow1")) {
+            let firstImage = btm1.childNodes[0].getBoundingClientRect();
+            slidingIndex > 0 ? slidingIndex-- : 0;
+            if (slidingIndex == 0) {
+                gsap.to("#left", {
+                    display: "none",
+                })
+            }
+            gsap.to("#btm1", {
+                x: `-${firstImage.width * slidingIndex}`
+            })
+        }
+    })
+})
+
+const video = document.querySelector("#video");
+
+function videoPlayBack() {
+    btm1.addEventListener("click", function (image) {
+        if (image.target.classList == "align") {
+            currentIndex = image.target.id;
+            blur()
+            select();
+            coming();
+            clearInterval(interval);
+            gsap.to("#videoLayer", {
+                opacity: 1,
+                pointerEvent: "all",
+                duration: 2,
+                ease: "Power4.easeInOut",
+                function() {
+                    gsap.to("#volumeControle", {
+                        display: "block"
+                    })
+                    setTimeout(() => {
+                        video.src = `${arr[image.target.id].video}`
+                        video.play();
+                    }, 1000)
+                }
+            })
+        }
+    })
+}
+videoPlayBack();
+video.addEventListener("play", function () {
+    gsap.to("#nameImage", {
+        opacity: .6,
+    })
+})
+
+video.addEventListener("ended", function () {
+    gsap.to("#videoLayer", {
+        opacity: 0,
+        pointerEvent: "none",
+        duration: 2,
+        ease: "Power4.easeInOut",
+    })
+    startInterval();
+})
+
+let volumeBtm = document.querySelector("#volumeControle");
+
+let mute = false;
+
+volumeBtm.addEventListener("click", function () {
+    if (mute == false) {
+        volumeBtm.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M792-56 671-177q-25 16-53 27.5T560-131v-82q14-5 27.5-10t25.5-12L480-368v208L280-360H120v-240h128L56-792l56-56 736 736-56 56Zm-8-232-58-58q17-31 25.5-65t8.5-70q0-94-55-168T560-749v-82q124 28 202 125.5T840-481q0 53-14.5 102T784-288ZM650-422l-90-90v-130q47 22 73.5 66t26.5 96q0 15-2.5 29.5T650-422ZM480-592 376-696l104-104v208Zm-80 238v-94l-72-72H200v80h114l86 86Zm-36-130Z"/></svg>`;
+        video.muted = true;
+    }
+    else {
+        volumeBtm.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M560-131v-82q90-26 145-100t55-168q0-94-55-168T560-749v-82q124 28 202 125.5T840-481q0 127-78 224.5T560-131ZM120-360v-240h160l200-200v640L280-360H120Zm440 40v-322q47 22 73.5 66t26.5 96q0 51-26.5 94.5T560-320ZM400-606l-86 86H200v80h114l86 86v-252ZM300-480Z"/></svg>`;
+        video.muted = false;
+    }
+    mute = !mute;
+})
